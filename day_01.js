4e25bca940c67c09d569e67bec63da2af520d6d9
Ram <96756162+ramkumarchauhan@users.noreply.github.com>
2024-02-23 18:08:11 +0000

    Day_01 Practice

--- day_01.js
+++ day_01.js
@@ -3,7 +3,7 @@
     1. Variable declaration - let, var, const
     2. Data Types - numbers, strings, booleans, null, undefined
     3. Condiotionals - if, if-else, mutli-conditional if-else
-    4. Loops - while, do-while, for loops --> Lopps continue executing block of code untill a terminating condition meets.
+    4. Loops - while, do-while, for loops --> Lopps continue executing block of code until a termination condition meets.
     5. Iterators - for...of, for...in --> Iterators on each available element of array or object
 */
 
@@ -49,6 +49,9 @@
 // let text = "It is a simple text line.";
 // console.log(typeof text);
 
+// let txt = "This is a simple text."
+// console.log(txt.length);
+
 // let isUploaded = false;
 // console.log(typeof isUploaded);
 
@@ -64,27 +67,87 @@
 
 /* 3. Condiotionals - if, if-else, mutli-conditional if-else------------------- */
 
-let num = 4;
-if(num< 5){
-    console.log("Number is less than 5.");
-}
+// let num = 4;
+// if(num< 5){
+//     console.log("Number is less than 5.");
+// }
 
-num = 12;
-if(num < 10){
-    console.log("Number is less than ")
-}else{
-    console.log("Number is greater than 10.")
-}
+// num = 12;
+// if(num < 10){
+//     console.log("Number is less than ")
+// }else{
+//     console.log("Number is greater than 10.")
+// }
 
-num = 0;
-if(num==0){
-    console.log("Number is equal to 0.")
-}else if(num == 15){
-    console.log("Number is equal to 15.");
-}else{
-    console.log("Number is greater than 15.")
-}
+// num = 0;
+// if(num==0){
+//     console.log("Number is equal to 0.")
+// }else if(num == 15){
+//     console.log("Number is equal to 15.");
+// }else{
+//     console.log("Number is greater than 15.")
+// }
 
+/* 4. Loops - while, do-while, for loops ------------ */
 
+// for(let count = 1; count <= 10; count++){
+//     console.log(count);
+// }
 
+// for(let i = 1; i <= 10; i++){
+//     console.log(4*i);
+// }
 
+// for(let m = 2; m != 8; ++m){
+//     console.log(m);
+// }
+
+// let num = 20;
+// while(num != 0 ){
+//     console.log(num);
+//     num = num - 1;
+// }
+
+//-----------Factorial Program--------------------
+// let number = 10;
+// let factorial = 1;
+// while(number != 1){
+//     factorial = number * factorial;
+//     number--;
+// }
+// console.log("Factorial is: "+ factorial);
+
+// Sum of first n numbers--------------------------
+// let m = 10;
+// let sum = 0;
+// do{
+//     sum = m + sum;
+//     m--;
+// }while(m > 0)
+// console.log("Sum is : "+ sum);
+
+/* 5. Iterators - for...of(With arrays), for...in(with Objects)-----------------------*/
+
+// const onlinePay = ["PhonePay","Paytm","GooglePay"];
+// for (const items of onlinePay) {
+//     console.log(items);
+// }
+
+// let str = "Pineapples";
+// for (char of str){
+//     if(char == 'p'){
+//         break;
+//     }
+//     console.log(char);
+// }
+
+// const bookDetails = {
+//     bookName : "Personality Traits",
+//     author : "P. William",
+//     price: 45.08,
+//     pubYear : 2018
+// }
+
+// for (const key in bookDetails){
+//     console.log(`${key} : ${bookDetails[key]}`);
+// }
