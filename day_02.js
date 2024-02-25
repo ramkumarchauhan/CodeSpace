ba1d55976b1d4d5677de59f562058a405aa895dd
Ram <96756162+ramkumarchauhan@users.noreply.github.com>
2024-02-25 09:15:37 +0000

    day_02 Practice

---
+++ day_02.js
@@ -0,0 +1,15 @@
+/*
+    1. String Methods 
+        - concat() -> joins two strings.
+        - charAt(index) -> returns the index number of mentioned character
+        - toUpperCase() -> Change all leters to Capital case.
+        - toLowerCase() -> Change all leters to Small case.
+        - trim() -> Removes whitespace from both ends of a string.
+        - split(",") -> Splits a string into an array of substrings based on the specified separator.
+        - replace(oldValue, newValue) -> Returns a new string replacing old value with new one.
+        - includes(searchWord) -> Determines whether one string may be found within another string
+        - startsWith(searchWord) -> Checks if a string starts with the specified string.
+        - endsWith(searchWord) -> Checks if a string ends with the specified string.
+        - sllice(startIndex, endIndex) -> Extracts a section of a string and returns it as a new string.   
+*/
+
