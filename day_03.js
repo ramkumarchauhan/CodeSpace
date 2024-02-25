77e76026624733e025531b3916ba2b0ab8254421
Ram <96756162+ramkumarchauhan@users.noreply.github.com>
2024-02-25 10:34:00 +0000

    Day_03 Practice

---
+++ day_03.js
@@ -0,0 +1,18 @@
+/*
+    Array & Array Methods
+    - push() -> Adds one or more elements to the end of an array and returns the new length of the array.
+    - pop() -> Removes the last element from an array and returns that element.
+    - concat() -> Returns a new array that combines the elements of the original array with other arrays or values.
+    - reverse() -> Reverses an array in place and returns the reference to the same array.
+    - sort() -> Sorts the elements of an array in place and returns the reference to the same array.
+    - toString() -> Returns a string representing the object.
+    - forEach() -> Executes a callback function once for each array element.
+    - map() -> Creates a new array with the results of calling a provided function on every element in the calling array.
+    - filter() -> Creates a new array with all elements that pass the test implemented by the provided function.
+    - slice() -> Returns a shallow copy of a portion of an array into a new array object selected from begin to end (end not included) where begin and end represent the index of items in that array. The original array will not be modified.
+    - splice() -> Changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
+    - shift() -> Removes the first element from an array.
+    - unshift() -> Inserts the given values to the beginning of an array-like object.
+    - join() - Joins the array’s items into the string and returns that string.
+
+*/
